8e51849f5e942353f5a1adc62adf10bdfdff3af3
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-12-27 16:37:15 +0530

    blog section and heading added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
-import './App.css';
+import Main from './Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
+        <h1>
+        React Blog
+        </h1>
       </header>
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Main() {
+  return (
+    <main>
+        <section>
+            <h2>Blog 1</h2>
+            <article>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non maxime illum eos quis id ex laboriosam laudantium molestiae vel consequatur ab veritatis, cumque optio sapiente minima atque omnis eaque assumenda?
+            Distinctio enim possimus doloribus asperiores, quaerat, ratione fugit fugiat dignissimos facilis vitae reiciendis. Asperiores cumque est accusantium quibusdam maiores natus impedit ducimus consequuntur suscipit, explicabo minima assumenda omnis! Sapiente, recusandae!
+            Commodi vitae libero dignissimos laudantium temporibus deleniti ea reiciendis exercitationem assumenda consequatur repellendus veniam hic facilis nisi deserunt ipsam delectus fugit et culpa voluptatum necessitatibus, ipsum sequi placeat? A, nihil!
+            Eius architecto fugit quis rerum corrupti error modi incidunt est! Velit temporibus voluptatem dicta necessitatibus. Magni deserunt neque, ipsam voluptatibus ullam natus voluptatem incidunt iure in nisi, temporibus, iste beatae.
+            Ex, vitae minus? Cumque voluptate porro facere, quibusdam minima, itaque laborum nisi alias eum veritatis doloribus corrupti, maxime adipisci autem amet consequatur fugit reiciendis facilis aut praesentium nam ipsam? Amet.
+            </article>
+        </section>
+        <section>
+            <h2>Blog 1</h2>
+            <article>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non maxime illum eos quis id ex laboriosam laudantium molestiae vel consequatur ab veritatis, cumque optio sapiente minima atque omnis eaque assumenda?
+            Distinctio enim possimus doloribus asperiores, quaerat, ratione fugit fugiat dignissimos facilis vitae reiciendis. Asperiores cumque est accusantium quibusdam maiores natus impedit ducimus consequuntur suscipit, explicabo minima assumenda omnis! Sapiente, recusandae!
+            Commodi vitae libero dignissimos laudantium temporibus deleniti ea reiciendis exercitationem assumenda consequatur repellendus veniam hic facilis nisi deserunt ipsam delectus fugit et culpa voluptatum necessitatibus, ipsum sequi placeat? A, nihil!
+            Eius architecto fugit quis rerum corrupti error modi incidunt est! Velit temporibus voluptatem dicta necessitatibus. Magni deserunt neque, ipsam voluptatibus ullam natus voluptatem incidunt iure in nisi, temporibus, iste beatae.
+            Ex, vitae minus? Cumque voluptate porro facere, quibusdam minima, itaque laborum nisi alias eum veritatis doloribus corrupti, maxime adipisci autem amet consequatur fugit reiciendis facilis aut praesentium nam ipsam? Amet.
+            </article>
+        </section>
+        <section>
+            <h2>Blog 1</h2>
+            <article>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non maxime illum eos quis id ex laboriosam laudantium molestiae vel consequatur ab veritatis, cumque optio sapiente minima atque omnis eaque assumenda?
+            Distinctio enim possimus doloribus asperiores, quaerat, ratione fugit fugiat dignissimos facilis vitae reiciendis. Asperiores cumque est accusantium quibusdam maiores natus impedit ducimus consequuntur suscipit, explicabo minima assumenda omnis! Sapiente, recusandae!
+            Commodi vitae libero dignissimos laudantium temporibus deleniti ea reiciendis exercitationem assumenda consequatur repellendus veniam hic facilis nisi deserunt ipsam delectus fugit et culpa voluptatum necessitatibus, ipsum sequi placeat? A, nihil!
+            Eius architecto fugit quis rerum corrupti error modi incidunt est! Velit temporibus voluptatem dicta necessitatibus. Magni deserunt neque, ipsam voluptatibus ullam natus voluptatem incidunt iure in nisi, temporibus, iste beatae.
+            Ex, vitae minus? Cumque voluptate porro facere, quibusdam minima, itaque laborum nisi alias eum veritatis doloribus corrupti, maxime adipisci autem amet consequatur fugit reiciendis facilis aut praesentium nam ipsam? Amet.
+            </article>
+        </section>
+        <section>
+            <h2>Blog 1</h2>
+            <article>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non maxime illum eos quis id ex laboriosam laudantium molestiae vel consequatur ab veritatis, cumque optio sapiente minima atque omnis eaque assumenda?
+            Distinctio enim possimus doloribus asperiores, quaerat, ratione fugit fugiat dignissimos facilis vitae reiciendis. Asperiores cumque est accusantium quibusdam maiores natus impedit ducimus consequuntur suscipit, explicabo minima assumenda omnis! Sapiente, recusandae!
+            Commodi vitae libero dignissimos laudantium temporibus deleniti ea reiciendis exercitationem assumenda consequatur repellendus veniam hic facilis nisi deserunt ipsam delectus fugit et culpa voluptatum necessitatibus, ipsum sequi placeat? A, nihil!
+            Eius architecto fugit quis rerum corrupti error modi incidunt est! Velit temporibus voluptatem dicta necessitatibus. Magni deserunt neque, ipsam voluptatibus ullam natus voluptatem incidunt iure in nisi, temporibus, iste beatae.
+            Ex, vitae minus? Cumque voluptate porro facere, quibusdam minima, itaque laborum nisi alias eum veritatis doloribus corrupti, maxime adipisci autem amet consequatur fugit reiciendis facilis aut praesentium nam ipsam? Amet.
+            </article>
+        </section>
+        <section>
+            <h2>Blog 1</h2>
+            <article>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non maxime illum eos quis id ex laboriosam laudantium molestiae vel consequatur ab veritatis, cumque optio sapiente minima atque omnis eaque assumenda?
+            Distinctio enim possimus doloribus asperiores, quaerat, ratione fugit fugiat dignissimos facilis vitae reiciendis. Asperiores cumque est accusantium quibusdam maiores natus impedit ducimus consequuntur suscipit, explicabo minima assumenda omnis! Sapiente, recusandae!
+            Commodi vitae libero dignissimos laudantium temporibus deleniti ea reiciendis exercitationem assumenda consequatur repellendus veniam hic facilis nisi deserunt ipsam delectus fugit et culpa voluptatum necessitatibus, ipsum sequi placeat? A, nihil!
+            Eius architecto fugit quis rerum corrupti error modi incidunt est! Velit temporibus voluptatem dicta necessitatibus. Magni deserunt neque, ipsam voluptatibus ullam natus voluptatem incidunt iure in nisi, temporibus, iste beatae.
+            Ex, vitae minus? Cumque voluptate porro facere, quibusdam minima, itaque laborum nisi alias eum veritatis doloribus corrupti, maxime adipisci autem amet consequatur fugit reiciendis facilis aut praesentium nam ipsam? Amet.
+            </article>
+        </section>
+        <section>
+            <h2>Blog 1</h2>
+            <article>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non maxime illum eos quis id ex laboriosam laudantium molestiae vel consequatur ab veritatis, cumque optio sapiente minima atque omnis eaque assumenda?
+            Distinctio enim possimus doloribus asperiores, quaerat, ratione fugit fugiat dignissimos facilis vitae reiciendis. Asperiores cumque est accusantium quibusdam maiores natus impedit ducimus consequuntur suscipit, explicabo minima assumenda omnis! Sapiente, recusandae!
+            Commodi vitae libero dignissimos laudantium temporibus deleniti ea reiciendis exercitationem assumenda consequatur repellendus veniam hic facilis nisi deserunt ipsam delectus fugit et culpa voluptatum necessitatibus, ipsum sequi placeat? A, nihil!
+            Eius architecto fugit quis rerum corrupti error modi incidunt est! Velit temporibus voluptatem dicta necessitatibus. Magni deserunt neque, ipsam voluptatibus ullam natus voluptatem incidunt iure in nisi, temporibus, iste beatae.
+            Ex, vitae minus? Cumque voluptate porro facere, quibusdam minima, itaque laborum nisi alias eum veritatis doloribus corrupti, maxime adipisci autem amet consequatur fugit reiciendis facilis aut praesentium nam ipsam? Amet.
+            </article>
+        </section>
+    </main>
+  );
+}
+
+export default Main;
